5bd5bc4328614670db4bd1ff7c2db11b2f3055d5
Subrat Mishra <piyush.subrat8@gmail.com>
2025-12-23 01:12:22 +0530

    instalation process

---
+++ docker/docker-3/dist/index.js
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import express from "express";
+const app = express();
+app.use(express.json());
+const prismaClient = new PrismaClient();
+app.post("/", async (req, res) => {
+    const { username, password } = req.body;
+    const resposne = await prismaClient.user.create({
+        data: {
+            username: username,
+            password: password
+        }
+    });
+    res.json({
+        data: resposne,
+        message: "post endpoint"
+    });
+});
+app.get("/", async (req, res) => {
+    const resposne = await prismaClient.user.findMany();
+    res.json({
+        allUsers: resposne,
+        message: "get endpoint"
+    });
+});
+app.listen(3000);
+//# sourceMappingURL=index.js.map
