b1c7d6cf2156171376ee1382273d22162ad5547f
Subrat Mishra <piyush.subrat8@gmail.com>
2026-01-01 20:07:52 +0530

    aws

--- week-29-1/index.ts
+++ week-29-1/index.ts
@@ -1,19 +1,161 @@
-import dotenv from "dotenv";
-import { AutoScalingClient, SetDesiredCapacityCommand } from "@aws-sdk/client-auto-scaling";
+import express from "express";
+import { AutoScalingClient, SetDesiredCapacityCommand, DescribeAutoScalingInstancesCommand, TerminateInstanceInAutoScalingGroupCommand } from "@aws-sdk/client-auto-scaling";
+const { EC2Client, DescribeInstancesCommand } = require("@aws-sdk/client-ec2");
 
-dotenv.config();
+const app = express();
+const client = new AutoScalingClient({ region: "ap-south-1", credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_ACCESS_SECRET!,
+} });
 
-const client = new AutoScalingClient({region: "ap-south-1", credentials: {
-    accessKeyId:process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY!
-}});
+const ec2Client = new EC2Client({ region: "ap-south-1", credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY!,
+    secretAccessKey: process.env.AWS_ACCESS_SECRET!,
+}})
 
+type Machine = {
+    ip: string;
+    isUsed: boolean;
+    assignedProject?: string;
+}
 
-const command = new SetDesiredCapacityCommand({
-    AutoScalingGroupName:"vscode-autoscaling-grp",
-    DesiredCapacity: 1
+const ALL_MACHINES: Machine[] = [];
+
+async function refershInstances() {
+    const command = new DescribeAutoScalingInstancesCommand();
+    const data = await client.send(command);
+
+    const ec2InstanceCommand = new DescribeInstancesCommand({
+        InstanceIds: data.AutoScalingInstances?.map(x => x.InstanceId)
+    })
+
+    const ec2Response = await ec2Client.send(ec2InstanceCommand);
+    // console.log(JSON.stringify(ec2Response.Reservations[0].Instances[0].PublicDnsName))
+    // TODO: Enrich the ALL_MACHINES array with the new instances, and remove the instances that have died
+}
+
+refershInstances();
+
+setInterval(() => {
+    refershInstances();
+}, 10 * 1000);
+
+app.get("/:projectId", (req, res) => {
+    const idleMachine = ALL_MACHINES.find(x => x.isUsed === false);
+    if (!idleMachine) {
+        // scale up the infrasturcture
+        res.status(404).send("No idle machine found");
+        return;
+    }
+
+    idleMachine.isUsed = true;
+    // scale up the infrasturcture
+
+    const command = new SetDesiredCapacityCommand({
+        AutoScalingGroupName: "vscode-asg",
+        DesiredCapacity: ALL_MACHINES.length + (5 - ALL_MACHINES.filter(x => x.isUsed === false).length)
+
+    })
+
+    client.send(command);
+
+    res.send({
+        ip: idleMachine.ip
+    });
 })
 
-const data = await client.send(command);
+app.post("/destroy", (req, res) => {
+    const machineId: string = req.body.machineId;
 
-console.log(data);
+    const command = new TerminateInstanceInAutoScalingGroupCommand({
+        InstanceId: machineId,
+        ShouldDecrementDesiredCapacity: true
+    })
+
+    client.send(command);
+})
+
+app.listen(9092);
+
+
+
+
+
+////////////////////
+
+
+// import dotenv from "dotenv";
+// import { AutoScalingClient, SetDesiredCapacityCommand, DescribeAutoScalingInstancesCommand } from "@aws-sdk/client-auto-scaling";
+// import express from "express";
+// import { EC2Client, DescribeInstancesCommand } from "@aws-sdk/client-ec2";
+// dotenv.config();
+
+// const app = express();
+
+// const client = new AutoScalingClient({region: "ap-south-1", credentials: {
+//     accessKeyId:process.env.AWS_ACCESS_KEY_ID!,
+//     secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY!
+// }});
+
+// const ec2Client = new EC2Client({region: "ap-south-1", credentials: {
+//     accessKeyId:process.env.AWS_ACCESS_KEY_ID!,
+//     secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY!
+// }});
+
+// type Machine = {
+//     ip: string,
+//     isUsed: boolean,
+//     assignedProject?: string
+// }
+
+// const ALL_MACHINES: Machine[] = [];
+
+// const refreshInstance = async () => {
+//     const command = new DescribeAutoScalingInstancesCommand();
+//     const data = await client.send(command);
+//     ALL_MACHINES.push(data?.AutoScalingInstances)
+
+//     const ec2InstanceCommand = new DescribeInstancesCommand({
+//         InstanceIds: data.AutoScalingInstances?.map(x => x.InstanceId)
+//     });
+
+//     const ec2Response = await ec2Client.send(ec2InstanceCommand);
+//     console.log("ec2Response", ec2Response.Reservations[0]?.Instances[0].PublicDnsName);
+// };
+
+// setInterval(() => {
+//     refreshInstance();
+// }, 10 * 1000);
+
+// app.get("/:projectId", (req, res) => {
+//     const {projectId} = req.params;
+//     const idleMachien = ALL_MACHINES.find((x: { isUsed: boolean; }) => x.isUsed === false);
+//     if(!idleMachien) {
+//         res.status(400).json({
+//             message:"No idle machine present"   //if machine is not available scale up here
+//         })
+//         return;
+//     }
+//     idleMachien.isUsed = true;
+
+
+//     res.send("hello world");
+// })
+
+// app.listen(3000);
+
+
+// // const client = new AutoScalingClient({region: "ap-south-1", credentials: {
+// //     accessKeyId:process.env.AWS_ACCESS_KEY_ID!,
+// //     secretAccessKey:process.env.AWS_SECRET_ACCESS_KEY!
+// // }});
+
+
+// // const command = new SetDesiredCapacityCommand({
+// //     AutoScalingGroupName:"vscode-autoscaling-grp",
+// //     DesiredCapacity: 1
+// // })
+
+// // const data = await client.send(command);
+
+// // console.log(data);
